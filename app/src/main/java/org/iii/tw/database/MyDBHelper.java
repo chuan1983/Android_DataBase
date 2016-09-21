b7760e07f79ad88ff312b529dcdf9690c628862f
chuan1983 <chuan1983126@gmail.com>
2016-09-21 09:52:14 +0800

    v4

--- app/src/main/java/org/iii/tw/database/MyDBHelper.java
+++ app/src/main/java/org/iii/tw/database/MyDBHelper.java
@@ -7,7 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by user on 2016/9/21.
  */
-public class MyDBHelper extends SQLiteOpenHelper{         //是建立資料表   先建立建構式  兩個抽象類別方法
+public class MyDBHelper extends SQLiteOpenHelper{         //是建立資料表   因為沒有無傳參數建構式所以要先建立建構式  兩個抽象類別方法
+
+    private  final String createTabe1 =
+            "CREATE TABLE cust (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    ""+"cname TEXT,tel TEXT,birthday DATE)";
+
     public MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
